8b995453d3191e489ca4b2c90839837c01c71089
Kelly Nezat <Soullab1@gmail.com>
2025-04-28 18:43:45 -0400

    fix: relocate autoSave.ts into src/utils for correct import

--- utils/shuffleArray.ts
+++
@@ -1,8 +0,0 @@
-// utils/shuffleArray.ts
-export function shuffleArray(array: any[]) {
-    return array
-      .map(value => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value);
-  }
-  
